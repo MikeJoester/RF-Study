a206bcc10bee302ad008c2f63e54ae2ba28d65df
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2023-12-24 18:01:45 +0700

    update dashboard

--- admin-dashboard/frontend/src/pages/dashboard/home.jsx
+++ admin-dashboard/frontend/src/pages/dashboard/home.jsx
@@ -20,9 +20,11 @@ import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 export function Home() {
   return (
     <div className="mt-12">
+      <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+        Add new student
+      </button>      
       <div className="mb-4 gap-6 xl:grid-cols-3">
-        <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
-          
+        <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">    
           <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
             <table className="w-full table-auto">
               <thead>
