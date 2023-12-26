99748e2bce13a612a6450678152b9a7fcbfc76f7
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2023-12-27 00:45:08 +0700

    Update && Add: CRUD Devices

--- admin-dashboard/frontend/src/API/user.js
+++ admin-dashboard/frontend/src/API/user.js
@@ -63,9 +63,87 @@ async function deleteUser(id) {
   }
 }
 
+async function getLogUser(id) {
+  try {
+    const response = await fetch(`${rootUrl}/api/std/${id}/logs`)
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error('Error in getUserInfo: ', e.message);
+  }
+}
+
+async function getDevicesInfo(id) {
+  try {
+    let response;
+    if (id) {
+      response = await fetch(`${rootUrl}/api/device/${id}`)
+    } else {
+      response = await fetch(`${rootUrl}/api/device`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error('Error in getUserInfo: ', e.message);
+  }
+}
+
+async function createDevice(formData) {
+  try {
+    const response = await fetch(`${rootUrl}/api/device/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    console.log('Create success');
+    return response.json();
+  } catch (e) {
+    console.error('Error in createUser: ', e.message);
+  }
+}
+
+async function updateDevice(formData, id) {
+  try {
+    const response = await fetch(`${rootUrl}/api/device/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+
+    console.log('>> response update: ', formData)
+    return response.json();
+  } catch (e) {
+    console.error('Error in updateUser: ', e.message);
+  }
+}
+
+async function deleteDevice(id) {
+  try {
+    const response = await fetch(`${rootUrl}/api/device/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    console.log('> delete user success')
+    return response
+  } catch (e) {
+    console.log('failed to delete')
+  }
+}
+
 export {
   getUserInfo,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  getLogUser,
+  getDevicesInfo,
+  createDevice,
+  updateDevice,
+  deleteDevice,
 };
