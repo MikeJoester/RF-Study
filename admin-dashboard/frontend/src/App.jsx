0f12ad0a00d288f1592bbba9e4edaed505ab03e7
Hiếu Lê <hieule@Hieus-MacBook-Air.local>
2023-12-28 15:00:56 +0700

    feat: fix routes

--- admin-dashboard/frontend/src/App.jsx
+++ admin-dashboard/frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 import StudentDashboard from "./pages/dashboard/student-dashboard";
+import { SignIn } from "./pages/auth";
 
 function App() {
   return (
     <Routes>
-      <Route path="/student-dashboard" element = {<StudentDashboard/>}/>
+      <Route path="/" element = {<StudentDashboard/>}/>
       <Route path="/home/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/student-dashboard" replace />} />
+      <Route path="/sign-in" element={<SignIn/>}/>
     </Routes>
   );
 }
